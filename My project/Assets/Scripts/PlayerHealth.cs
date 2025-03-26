032f212be09f8cde301291acfd6b926fee7e4bf4
KhaladOsman <kosman7@uwo.ca>
2025-03-26 13:56:12 -0400

    Implmemted logic to update health text as player takes damage

--- My project/Assets/Scripts/PlayerHealth.cs
+++ My project/Assets/Scripts/PlayerHealth.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
+    private TMP_Text healthText;
 
     void Start()
     {
         currentHealth = maxHealth; // Set health to max at the start
+        
+        GameObject healthObject = GameObject.FindGameObjectWithTag("healthTag");
+        if (healthObject != null)
+        {
+            healthText = healthObject.GetComponent<TMP_Text>();
+            UpdateHealthUI();
+        }
+        else
+        {
+            Debug.LogWarning("No TextMeshPro object with tag 'healthTag' found!");
+        }
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        currentHealth = Mathf.Max(currentHealth, 0);
         Debug.Log("Player Health: " + currentHealth);
+        
+        UpdateHealthUI();
 
         if (currentHealth <= 0)
         {
@@ -35,5 +51,15 @@ public class PlayerHealth : MonoBehaviour
             currentHealth = maxHealth;
         }
         Debug.Log("Player healed: " + currentHealth);
+        
+        UpdateHealthUI();
+    }
+    
+    private void UpdateHealthUI()
+    {
+        if (healthText != null)
+        {
+            healthText.text = currentHealth.ToString();
+        }
     }
 }
